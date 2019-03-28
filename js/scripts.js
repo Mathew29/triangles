f596b383c30b02bc44a66f58dc8b3d44538ff6cd
Kaya Jepson and Mathew Akre <kaya@>
2019-03-28 14:27:17 -0700

    refactor code to make parseInt functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
 $(document).ready(function(){
   $("#user").submit(function(event) {
     event.preventDefault();
-    var side1Input = $("input#side1").val();
-    var side2Input = $("input#side2").val();
-    var side3Input = $("input#side3").val();
+    var side1Input = parseInt($("input#side1").val());
+    var side2Input = parseInt($("input#side2").val());
+    var side3Input = parseInt($("input#side3").val());
 
     if (side1Input === side2Input && side1Input === side3Input) {
-    $("#equilateral").show();
-  } else if (side1Input === side2Input && side1Input > side3Input) {
-    $("#isosceles").show();
-  } else if (side1Input === side3Input && side1Input > side2Input) {
-    $("#isosceles").show();
-  } else if (side2Input === side3Input && side2Input > side1Input) {
-    $("#isosceles").show();
-  } else if (side1Input != side2Input && side2Input != side3Input && side1Input != side3Input) {
-    $("#scalene").show();
-  } else if (side1Input + side2Input <= side3Input || side2Input + side3Input <= side1Input || side1Input + side3Input <= side2Input) {
-    $("#not-a-triangle").show();
-  } else {
-    alert("Invalid input try again");
-    return;
-  }
-
+      $("#equilateral").show();
+    } else if (side1Input === side2Input && side1Input > side3Input) {
+      $("#isosceles").show();
+    } else if (side1Input === side3Input && side1Input > side2Input) {
+      $("#isosceles").show();
+    } else if (side2Input === side3Input && side2Input > side1Input) {
+      $("#isosceles").show();
+    } else if (side1Input != side2Input && side2Input != side3Input && side1Input != side3Input) {
+      $("#scalene").show();
+    } else if (side1Input + side2Input <= side3Input || side2Input + side3Input <= side1Input || side1Input + side3Input <= side2Input) {
+      $("#not-a-triangle").show();
+    } else {
+      alert("Invalid input try again");
+      return;
+    }
   });
 
 
