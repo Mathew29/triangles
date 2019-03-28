339d4b8abd84440799efbcec20726887df1eaeb7
Kaya Jepson and Mathew Akre <mathew@>
2019-03-28 13:49:36 -0700

    add css display none, js #not-a-triangle, fix input mistakes in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
   $("#user").submit(function(event) {
     event.preventDefault();
-    var side1Input = $("input:#side1").val();
-    var side2Input = $("input:#side2").val();
-    var side3Input = $("input:#side3").val();
+    var side1Input = $("input#side1").val();
+    var side2Input = $("input#side2").val();
+    var side3Input = $("input#side3").val();
 
     if (side1Input === side2Input && side1Input === side3Input) {
     $("#equilateral").show();
-  } else if (side1Input === side2Input && side1Input > sideInput3) {
+  } else if (side1Input === side2Input && side1Input > side3Input) {
     $("#isosceles").show();
-  } else if (side1Input === side3Input && side1Input > sideInput2) {
+  } else if (side1Input === side3Input && side1Input > side2Input) {
     $("#isosceles").show();
   } else if (side2Input === side3Input && side2Input > side1Input) {
     $("#isosceles").show();
-  } else if (side1Input !=== side2Input && side2Input !=== side3Input && side1Input !=== side3Input) {
+  } else if (side1Input != side2Input && side2Input != side3Input && side1Input != side3Input) {
     $("#scalene").show();
   } else if (side1Input + side2Input <= side3Input || side2Input + side3Input <= side1Input || side1Input + side3Input <= side2Input) {
     $("#not-a-triangle").show();
+  } else {
+    alert("Invalid input try again");
+    return;
   }
 
   });
